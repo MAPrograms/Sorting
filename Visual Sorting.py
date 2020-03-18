a8768da8ac324159bdf0669bcba59fc3cff668c1
Allan <allan.christiansen@live.dk>
2020-03-18 15:48:48 +0100

    added sortingAlgorithms class and selectsort function

--- Visual Sorting.py
+++ Visual Sorting.py
@@ -1,20 +1,25 @@
-array = [4, 2, 5, 7, 1, 3]
-array_sorted = []
-sort = True
-min = 0
 
-while sort:
-    for numbers in array:
-        if numbers > min:
-            min = numbers
-    if min != 0:
-        array_sorted.insert(0, min)
+class sortingAlgorithms:
 
-    if len(array) != 1:
-        array.remove(min)
+    def selectsort(self):
+        array = [4, 2, 5, 7, 1, 3]
+        array_sorted = []
+        sort = True
+        min = 0
+        while sort:
+            for numbers in array:
+                if numbers > min:
+                    min = numbers
+            if min != 0:
+                array_sorted.insert(0, min)
+
+            if len(array) != 1:
+                array.remove(min)
+                print(array_sorted)
+            else:
+                sort = False
+            min = 0
+
         print(array_sorted)
-    else:
-        sort = False
-    min = 0
 
-print(array_sorted)
+sortingAlgorithms.selectsort()
