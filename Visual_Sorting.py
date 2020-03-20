8d4a29cf60aac53f06835ef77e6a1ff9c0d1fc0e
AllanFC <allan.christiansen@live.dk>
2020-03-20 03:20:00 +0100

    Update Visual_Sorting.py

--- Visual_Sorting.py
+++ Visual_Sorting.py
@@ -5,10 +5,10 @@ class sortingAlgorithms:
     def newarray(self, array, bars, max): # Makes a new array
         array.clear()
         for i in range(bars):
-            array.insert(0, random.randint(1, max))
+            array.insert(0, random.randint(5, max))
 
 
-    def select(self, array): # Select Sort
+    def selection(self, array): # Select Sort
         array_sorted = []
         min = 0
         print(array)
@@ -26,79 +26,42 @@ class sortingAlgorithms:
 
     def bubble(self, array): # Bubble Sort
         a = len(array)
-        print(array)
         for i in range(a):
             for k in range(a - i - 1):
                 if array[k] > array[k+1]:
                     array[k], array[k+1] = array[k+1], array[k]
-            print(array)
+            break
+
+
 
 
     def cocktail(self, array): # Cocktail Sort
         a = len(array)
-        print(array)
         for i in range(a):
             for k in range(a - i - 1):
                 if array[k] > array[k+1]:
                     array[k], array[k+1] = array[k+1], array[k]
 
-
             for k in range(a - i - 1, 0, -1):
                 if array[k] < array[k-1]:
                     array[k], array[k-1] = array[k-1], array[k]
-            print(array)
+            break
 
 
 class bars:
 
-    def __init__(self, height=0, width=10, windowWidth=800, windowHeight=600):
+    def __init__(self, height=0, width=10, spacing=10, windowWidth=800, windowHeight=600, a=0):
         self.height = height
+        self.spacing = spacing
         self.width = width
-        self.x = windowWidth / 8
+        self.windowWidth = windowWidth
+        self.windowHeight = windowHeight
+        self.a = a
+        self.x = windowWidth / 2 - self. width / 2 - ((a + 1) * (width / 2 + self.spacing / 2))
         self.y = windowHeight - height
 
         self.bar = pygame.Surface((self.width, self.height))
-        self.bar.fill((255, 255, 255))
+        self.bar.fill((0, 162, 232))
 
     def render(self, window):
         window.blit(self.bar, (self.x, self.y))
-
-
-# Variables for color
-grey = (98, 98, 98)
-blue = (0, 162, 232)
-
-# Variables for the top bar
-topbarX = 0
-topbarY = 0
-topbarHeight = 70
-
-# Variables for underline bar
-underlinebarX = 0
-underlinebarY = 70
-underlinebarHeight = 3
-
-
-# Variables for the buttons
-btnHeight = 20
-btnWidth = 65
-
-# Variables for the sort array btn
-sortarraybtnX = 15
-sortarraybtnY = 25
-
-# Variables for new array btn
-newarraybtnX = 100
-newarraybtnY = 25
-
-# Create rectangles and renders them
-class rectangle:
-    def __init__(self, color, rect):
-        self.color = color
-        self.rectX = rect[0]
-        self.recty = rect[1]
-        self.width = rect[2]
-        self.height = rect[3]
-
-    def render(self, screen):
-        pygame.draw.rect(screen, self.color, (self.rectX, self.recty, self.width, self.height))
