1b3601dae2afdf06ee51d6af9d39cacafe479604
AllanFC <allan.christiansen@live.dk>
2020-03-20 03:19:06 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,12 +1,14 @@
 from Visual_Sorting import *
 
-done = False
 # Pygame variables
-windowWidth = 800
-windowHeight = 600
+windowWidth = 1366
+windowHeight = 768
 # Pygame stuff
 pygame.init()
 window = pygame.display.set_mode((windowWidth, windowHeight))
+
+FPS = 15
+clock = pygame.time.Clock()
 
 # Sorting Stuff
 array = [20, 30, 40, 50, 10]
@@ -14,14 +16,21 @@ sorting = sortingAlgorithms()
 
 # Bar stuff
 bar_list = []
-
-#sorting.newarray(array, 25, 100)
+sorting.newarray(array, 120, 400)
+#print(array)
+c = 0
+for i in array:
+    bar = bars(height=i, spacing=5, width=5, a=len(array), windowWidth=windowWidth, windowHeight=windowHeight)
+    bar_list.append(bar)
+    bar_list[c].x = bar_list[c - 1].x + bar_list[c].width + bar_list[c].spacing
+    c += 1
 
 #sorting.select(array)
 
 #sorting.bubble(array)
 
 #sorting.cocktail(array)
+
 
 while True:
     for event in pygame.event.get():
@@ -31,33 +40,22 @@ while True:
 
     window.fill((56, 56, 56))
 
-    # Creates the top bar
-    topbar = rectangle(grey, (topbarX, topbarY, windowWidth, topbarHeight))
-    topbar.render(window)
-
-    # Creates the underline bar
-    underlinebar = rectangle(blue, (underlinebarX, underlinebarY, windowWidth, underlinebarHeight))
-    underlinebar.render(window)
-
-    # Creates the different buttons
-    sortarraybtn = rectangle(blue, (sortarraybtnX, sortarraybtnY, btnWidth, btnHeight))
-    sortarraybtn.render(window)
-
-    newarraybtn = rectangle(blue, (newarraybtnX, newarraybtnY, btnWidth, btnHeight))
-    newarraybtn.render(window)
-
-    if len(bar_list) != len(array):
-        for i in array:
-            bar = bars(height=i)
-            bar_list.append(bar)
-        done = True
+    sorting.bubble(array)
+    # for l in range(len(array)):
+    #     if array[l] != bar_list[l].height:
+    #         bar_list.clear()
+    #         c = 0
+    #         for i in array:
+    #             bar = bars(height=i, spacing=5, width=5, a=len(array), windowWidth=windowWidth, windowHeight=windowHeight)
+    #             bar_list.append(bar)
+    #             bar_list[c].x = bar_list[c - 1].x + bar_list[c].width + bar_list[c].spacing
+    #             c += 1
+    for l in range(len(array)):
+        if array[l] != bar_list[l].height:
+            bar_list[l].x, bar_list[l].x = bar_list[l].x, bar_list[l].x
 
     for j in range(len(bar_list)):
-        if done == True:
-            bar_list[j].x = bar_list[j-1].x + bar_list[j].width + 5
         bar_list[j].render(window)
 
-    done = False
-
     pygame.display.update()
-
+    clock.tick(FPS)
